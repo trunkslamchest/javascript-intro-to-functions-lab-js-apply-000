be00b1233b6ab9aafa7b07ed0fe8dba046a0d7eb
Austin Smith <trunk9slamchest@gmail.com>
2019-06-25 16:03:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return 'YES INDEED!'
   }
   else if (string === string.toLowercase()) {
-    return 'YES INDEED!'
+    return "I can't hear you!"
   }
   else {
     return 'I love you, too.'
