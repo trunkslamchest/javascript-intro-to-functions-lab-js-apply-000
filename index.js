8930b0d4af7898e692a164e330b536c45f0680f9
Austin Smith <trunk9slamchest@gmail.com>
2019-06-25 16:00:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function shout(string) {
-  
+  return string.toUppercase();
 }
 
 function whisper(string) {
-  
+  return string.toLowercase();
 }
 
 function logShout(string) {
-  
+  console.log(string.toUppercase())
 }
 
 function logWhisper(string) {
-  
+  console.log(string.toLowercase())
 }
 
 function sayHiToGrandma(string) {
